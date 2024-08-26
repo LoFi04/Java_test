02a4f69ad0537c53439f3d7e4667006bd93cc927
LoFi04 <mahuerk@gmail.com>
2024-08-25 19:15:22 -0600

    mi primer commit

---
+++ Calculadora/src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+
+}
